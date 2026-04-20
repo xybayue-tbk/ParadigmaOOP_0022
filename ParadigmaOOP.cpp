a6b5352897711e46d64f1acccc29ca712e43534e
xybayue-tbk <xybayue@gmail.com>
2026-04-20 10:32:52 +0700

    menambahkan method

--- ParadigmaOOP.cpp
+++ ParadigmaOOP.cpp
@@ -6,4 +6,10 @@ class mahasiswa{
         int nim;
         string nama;
         float nilai;
+    
+        void printData(){
+    cout << "NIM   : " << nim << endl;
+    cout << "Nama  : " << nama << endl;
+    cout << "Nilai : " << nilai << endl;
+    }
 };
